482ac0ffc07d90ed837eca841dcbd6856c5d17ba
Joshua Mora <jshm101@gmail.com>
2018-07-09 01:44:16 -0700

    abstract db connection logic to dedicated script

---
+++ bin/utils/db-connect/index.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+const validDevEnv = require('../valid-dev-env')
+const validProdEnv = require('../valid-prod-env')
+const validTestEnv = require('../valid-test-env')
+
+const env = process.env.NODE_ENV || 'development'
+
+let MONGODB_CONNECTION_URI = ''
+let MONGODB_CONNECTION_USERNAME = ''
+let MONGODB_CONNECTION_PASSWORD = ''
+
+if (env === 'test' && validTestEnv()) {
+  const {
+    JR_SITE_TEST_DB_CONNECTION_URI,
+    JR_SITE_TEST_DB_CONNECTION_USERNAME,
+    JR_SITE_TEST_DB_CONNECTION_PASSWORD
+  } = process.env
+
+  MONGODB_CONNECTION_URI = JR_SITE_TEST_DB_CONNECTION_URI
+  MONGODB_CONNECTION_USERNAME = JR_SITE_TEST_DB_CONNECTION_USERNAME
+  MONGODB_CONNECTION_PASSWORD = JR_SITE_TEST_DB_CONNECTION_PASSWORD
+}
+
+if (env === 'development' && validDevEnv()) {
+  const {
+    JR_SITE_DEV_DB_CONNECTION_URI,
+    JR_SITE_DEV_DB_CONNECTION_USERNAME,
+    JR_SITE_DEV_DB_CONNECTION_PASSWORD
+  } = process.env
+
+  MONGODB_CONNECTION_URI = JR_SITE_DEV_DB_CONNECTION_URI
+  MONGODB_CONNECTION_USERNAME = JR_SITE_DEV_DB_CONNECTION_USERNAME
+  MONGODB_CONNECTION_PASSWORD = JR_SITE_DEV_DB_CONNECTION_PASSWORD
+}
+
+if (env === 'production' && validProdEnv()) {
+  const {
+    JR_SITE_PROD_DB_CONNECTION_URI,
+    JR_SITE_PROD_DB_CONNECTION_USERNAME,
+    JR_SITE_PROD_DB_CONNECTION_PASSWORD
+  } = process.env
+
+  MONGODB_CONNECTION_URI = JR_SITE_PROD_DB_CONNECTION_URI
+  MONGODB_CONNECTION_USERNAME = JR_SITE_PROD_DB_CONNECTION_USERNAME
+  MONGODB_CONNECTION_PASSWORD = JR_SITE_PROD_DB_CONNECTION_PASSWORD
+}
+
+const CONNECTION_STRING = (
+  `mongodb://${MONGODB_CONNECTION_USERNAME}:` +
+  `${encodeURIComponent(MONGODB_CONNECTION_PASSWORD)}@` +
+  `${MONGODB_CONNECTION_URI}`
+)
+
+module.exports = () => (
+  mongoose.connect(
+    CONNECTION_STRING,
+    { useNewUrlParser: true }
+  )
+)

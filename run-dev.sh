cb539ea16f8900f2b2a0128914727f283ae3d471
Joshua Mora <jshm101@gmail.com>
2018-07-15 21:25:50 -0700

    dockerize microservice (start with dev env)

---
+++ run-dev.sh
@@ -0,0 +1,5 @@
+docker kill jr-site-users-service-dev-container
+docker rm jr-site-users-service-dev-container
+docker build -f ./docker/dev/Dockerfile -t jr-site-users-service-dev --build-arg JR_SITE_DEV_DB_CONNECTION_URI=${JR_SITE_DEV_DB_CONNECTION_URI} --build-arg JR_SITE_DEV_DB_CONNECTION_USERNAME=${JR_SITE_DEV_DB_CONNECTION_USERNAME} --build-arg JR_SITE_DEV_DB_CONNECTION_PASSWORD=${JR_SITE_DEV_DB_CONNECTION_PASSWORD} .
+docker run -p 3005:3005 -d --name jr-site-users-service-dev-container jr-site-users-service-dev
+docker logs -f jr-site-users-service-dev-container

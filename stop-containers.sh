f4aa632c490bdcbd23a66f2b3c7e86a2a7165f5d
Joshua Mora <jshm101@gmail.com>
2018-07-17 13:52:14 -0700

    abstract container shutdown to dedicated script

---
+++ stop-containers.sh
@@ -0,0 +1,4 @@
+docker kill jr-site-users-service-dev-container
+docker rm jr-site-users-service-dev-container
+docker kill jr-site-users-service-prod-container
+docker rm jr-site-users-service-prod-container

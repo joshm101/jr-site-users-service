575050bffdc12f918c5a11abc4a487221298595d
Joshua Mora <jshm101@gmail.com>
2019-04-13 14:36:25 -0700

    provide environment variables during runtime instead of build-time

--- run-prod.sh
+++ run-prod.sh
@@ -1,4 +1,4 @@
 ./stop-containers.sh
-docker build -f ./docker/prod/Dockerfile -t jr-site-users-service-prod --build-arg JR_SITE_PROD_DB_CONNECTION_URI=${JR_SITE_PROD_DB_CONNECTION_URI} --build-arg JR_SITE_PROD_DB_CONNECTION_USERNAME=${JR_SITE_PROD_DB_CONNECTION_USERNAME} --build-arg JR_SITE_PROD_DB_CONNECTION_PASSWORD=${JR_SITE_PROD_DB_CONNECTION_PASSWORD} .
-docker run -p 3005:3005 -d --name jr-site-users-service-prod-container jr-site-users-service-prod
+docker build -f ./docker/prod/Dockerfile -t jr-site-users-service-prod .
+docker run --env JR_SITE_PROD_DB_CONNECTION_URI=${JR_SITE_PROD_DB_CONNECTION_URI} --env JR_SITE_PROD_DB_CONNECTION_USERNAME=${JR_SITE_PROD_DB_CONNECTION_USERNAME} --env JR_SITE_PROD_DB_CONNECTION_PASSWORD=${JR_SITE_PROD_DB_CONNECTION_PASSWORD} -p 3005:3005 -d --name jr-site-users-service-prod-container jr-site-users-service-prod
 docker logs -f jr-site-users-service-prod-container

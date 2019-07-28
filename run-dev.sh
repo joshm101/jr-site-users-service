4d1164ec278346718a33d312902e0f1b6b5780a7
Joshua Mora <jshm101@gmail.com>
2019-07-28 15:31:01 -0700

    update docker config

--- run-dev.sh
+++ run-dev.sh
@@ -1,4 +1,4 @@
 ./stop-containers.sh
-docker build -f ./docker/dev/Dockerfile -t jr-site-users-service-dev .
-docker run --env JR_SITE_DEV_DB_CONNECTION_URI=${JR_SITE_DEV_DB_CONNECTION_URI} --env JR_SITE_DEV_DB_CONNECTION_USERNAME=${JR_SITE_DEV_DB_CONNECTION_USERNAME} --env JR_SITE_DEV_DB_CONNECTION_PASSWORD=${JR_SITE_DEV_DB_CONNECTION_PASSWORD} -p 3005:3005 -d --name jr-site-users-service-dev-container jr-site-users-service-dev
+docker build -f ./docker/dev/Dockerfile -t jshm101/jr-site-users-dev .
+docker run --env JR_SITE_DEV_DB_CONNECTION_URI=${JR_SITE_DEV_DB_CONNECTION_URI} --env JR_SITE_DEV_DB_CONNECTION_USERNAME=${JR_SITE_DEV_DB_CONNECTION_USERNAME} --env JR_SITE_DEV_DB_CONNECTION_PASSWORD=${JR_SITE_DEV_DB_CONNECTION_PASSWORD} -p 3005:3005 -d --name jr-site-users-service-dev-container jshm101/jr-site:users-dev
 docker logs -f jr-site-users-service-dev-container
